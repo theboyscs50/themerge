43ccd855d9f6ef1666793863afaba28231eafe0d
Nathan Hollenberg <nathanhollenberg@Nathans-MacBook-Pro-4.local>
2016-11-29 16:50:26 -0500

    the real deal

--- Tour Harvard/ViewController.swift
+++ Tour Harvard/ViewController.swift
@@ -12,113 +12,41 @@ import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
-    @IBOutlet weak var mapView: MKMapView?
+    @IBOutlet weak var map: MKMapView!
     let locationManager = CLLocationManager()
 
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation = true
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         locationManager.delegate = self;
         locationManager.requestWhenInUseAuthorization()
-        locationManager.requestAlwaysAuthorization()
         locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters;
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        locationManager.startUpdatingLocation()
         
-        mapView?.delegate = self
-        mapView?.showsUserLocation = true
-        mapView?.userTrackingMode = .follow
-        
+        let wigg = MKPointAnnotation()
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(42.373076, -71.117164)
+        wigg.coordinate = location
+        wigg.title = "Wigglesworth Hall"
+        wigg.subtitle = "Click for More Information"
+        map.addAnnotation(wigg)
+    }
+    
 
 
-    }
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        // 1. status is not determined
-        if CLLocationManager.authorizationStatus() == .notDetermined {
-            locationManager.requestAlwaysAuthorization()
-        }
-            // 2. authorization were denied
-        else if CLLocationManager.authorizationStatus() == .denied {
-            showAlert(title: "Location services were previously denied. Please enable location services for this app in Settings.")
-        }
-            // 3. we do have authorization
-        else if CLLocationManager.authorizationStatus() == .authorizedAlways {
-            locationManager.startUpdatingLocation()
-        }
-    }
-    
-    
-   
-    func setupData() {
-        // 1. check if system can monitor regions
-        if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
-            
-            // 2. region data
-            let title = "Lorrenzillo's"
-            let coordinate = CLLocationCoordinate2DMake(37.703026, -121.759735)
-            let regionRadius = 300.0
-            
-            // 3. setup region
-            let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude,
-                                                                         longitude: coordinate.longitude), radius: regionRadius, identifier: title)
-            locationManager.startMonitoring(for: region)
-            
-            // 4. setup annotation
-            let restaurantAnnotation = MKPointAnnotation()
-            restaurantAnnotation.coordinate = coordinate;
-            restaurantAnnotation.title = "\(title)";
-            mapView?.addAnnotation(restaurantAnnotation)
-            
-            // 5. setup circle
-            let circle = MKCircle(center: coordinate, radius: regionRadius)
-            mapView?.add(circle)
-        }
-        else {
-            print("System can't track regions")
-        }
-    }
-    
-    // 6. draw circle
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        let circleRenderer = MKCircleRenderer(overlay: overlay)
-        circleRenderer.strokeColor = UIColor.red
-        circleRenderer.lineWidth = 1.0
-        return circleRenderer
-    }
-    
-    
-    // 1. user enter region
-    private func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        
-        
-    }
-    
-    // 2. user exit region
-    private func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
-        
-    }
-    
-    func showAlert(title: String) {
-        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
-            alert.dismiss(animated: true, completion: nil)
-        }))
-        self.present(alert, animated: true, completion: nil)
-        
-    }
-    
-   
 }
-
-
-
