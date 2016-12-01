3a9dd7f2a97f28172cfd37a516efb90657e8bb22
Nathan Hollenberg <nathanhollenberg@Nathans-MacBook-Pro-4.local>
2016-11-30 19:39:16 -0500

    gggg

--- Tour Harvard/ViewController.swift
+++ Tour Harvard/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
     }
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -39,14 +40,79 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
         locationManager.startUpdatingLocation()
         
+        map.delegate = self
+        map.showsUserLocation = true
+        map.userTrackingMode = .follow
+        
         let wigg = MKPointAnnotation()
         let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(42.373076, -71.117164)
         wigg.coordinate = location
         wigg.title = "Wigglesworth Hall"
         wigg.subtitle = "Click for More Information"
         map.addAnnotation(wigg)
+        
+        setupData()
+        
+        
+        }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 1. status is not determined
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }
+            // 2. authorization were denied
+        else if CLLocationManager.authorizationStatus() == .denied {
+            let alert = UIAlertController(title: "Alert", message: "Location services were previously denied. Please enable location services for this app in Settings.", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+            // 3. we do have authorization
+        else if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
     }
     
+    func setupData() {
+        // 1. check if system can monitor regions
+        if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
+            
+            // 2. region data
+            let title = "Lorrenzillo's"
+            let coordinate = CLLocationCoordinate2DMake(37.703026, -121.759735)
+            let regionRadius = 300.0
+            
+            // 3. setup region
+            let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude,
+                                                                         longitude: coordinate.longitude), radius: regionRadius, identifier: title)
+            locationManager.startMonitoring(for: region)
+            
+            // 4. setup annotation
+            let restaurantAnnotation = MKPointAnnotation()
+            restaurantAnnotation.coordinate = coordinate;
+            restaurantAnnotation.title = "\(title)";
+            map.addAnnotation(restaurantAnnotation)
+            
+            // 5. setup circle
+            let circle = MKCircle(center: coordinate, radius: regionRadius)
+            map.add(circle)
+        }
+        else {
+            print("System can't track regions")
+        }
+    }
+    
+    // 6. draw circle
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let circleRenderer = MKCircleRenderer(overlay: overlay)
+        circleRenderer.strokeColor = UIColor.red
+        circleRenderer.lineWidth = 1.0
+        return circleRenderer
+    }
+    
+
 
 
 }
